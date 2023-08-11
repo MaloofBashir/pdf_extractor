1fb1acbc2c578e19be7ed2fb7090c0bded999400
Maloof <maloof700@gmail.com>
2023-08-11 20:50:59 +0530

    updated

--- pdf/views.py
+++ pdf/views.py
@@ -8,40 +8,44 @@ import docx
 
 
 table_data=[]
+results=[]
+
+def get_table_data(df,sub_name):
+    for index,row in df.iterrows():
+        value=row["Subjects1"]
+        if sub_name in value:
+            value=df.loc[index,"ClassRollNo"]
+            if value.isdigit()!=True:
+                print(df.iloc[index])
+            
+            results.append(value+",")
+    return results
 
 # Create your views here.
 
 def display(request):
-    if request.method=="POST":
-        uploaded_file=request.FILES['document']
+    if request.method == "POST":
+        uploaded_file = request.FILES['document']
+        file_content = uploaded_file.read()
+        doc = Document(io.BytesIO(file_content))
 
-        with uploaded_file.open("r") as file:
-            
-            file_content=file.read()
     
-            print("file content: ", file_content)
+        for table in doc.tables:
+            # print("table is {table}")
+            for row in table.rows:
+                row_data=[]
+                for cell in row.cells:
+                    row_data.append(cell.text.strip())
+                table_data.append(row_data)
+        df=pd.DataFrame(table_data[1:],columns=table_data[0])
+        columns=df.columns
+
+        result=get_table_data(df,"ENG320")
+        
+        return render(request,"main.html",{"data":result,"columns":columns})
 
 
-        # doc_content=uploaded_file.read()
-        # doc=Document(io.BytesIO(doc_content))
-        # doc=Document(io.BytesIO(doc_content))
-        # print(doc)
 
-
-        # new_doc=doc.tables[1]
-        # print(new_doc)
     
-        # for table in doc.tables:
-        #     # print("table is {table}")
-        #     for row in table.rows:
-        #         row_data=[]
-        #         print(row)
-        #         for cell in row.cells:
-        #             row_data.append(cell.text.strip())
-        #         table_data.append(row_data)
-                # print(table_data)
-        # df=pd.DataFrame(table_data[1:],columns=table_data[0])
-
-        # print(df)
         
     return render(request,"main.html",{})
