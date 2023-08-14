8d3e5892c58bc1a0b4604cc9ab2cd22d139ac6f6
Maloof <maloof700@gmail.com>
2023-08-14 06:22:48 +0530

    updating the template

--- pdf/views.py
+++ pdf/views.py
@@ -4,19 +4,22 @@ from docx import Document
 import io
 import pandas as pd
 import docx
+from tabulate import tabulate
 
 
 
 table_data=[]
-results=[]
 
-def get_table_data(df,sub_name):
+def get_table_data(df,sub_name,col_name):
+    results=[]
     for index,row in df.iterrows():
-        value=row["Subjects1"]
+        value=row[col_name]
+        if index==2:
+            exit
         if sub_name in value:
             value=df.loc[index,"ClassRollNo"]
             if value.isdigit()!=True:
-                print(df.iloc[index])
+                results.append("--")
             
             results.append(value+",")
     return results
@@ -26,40 +29,46 @@ def extract_subjects(subject):
     sub=sub.split()
     return sub
 
+
+
+
 # Create your views here.
 
 def display(request):
     if request.method == "POST":
+        try:
+            uploaded_file = request.FILES['document']
+            subjects = request.POST.get("subject")
+            col_name=request.POST.get("col_name")
+            subjects = extract_subjects(subjects)
 
-        sub_wise_data={}
-        uploaded_file = request.FILES['document']
-        subjects=request.POST.get("subject")
-        subjects=extract_subjects(subjects)
+            file_content = uploaded_file.read()
+            doc = Document(io.BytesIO(file_content))
 
-        print(f"subjects typed are {subjects}")
-
-        file_content = uploaded_file.read()
-        doc = Document(io.BytesIO(file_content))
-
-    
-        for table in doc.tables:
-            # print("table is {table}")
-            for row in table.rows:
-                row_data=[]
-                for cell in row.cells:
-                    row_data.append(cell.text.strip())
-                table_data.append(row_data)
-        df=pd.DataFrame(table_data[1:],columns=table_data[0])
-        columns=df.columns
-        for sub in subjects:
-            data=get_table_data(df,sub)
-            sub_wise_data[sub]=data
-            results=[]
-            print(sub_wise_data)
-        return render(request,"main.html",{"sub_wise_data":sub_wise_data,"columns":columns})
+            table_data = []
+            for table in doc.tables:
+                for row in table.rows:
+                    row_data = []
+                    for cell in row.cells:
+                        row_data.append(cell.text.strip())
+                    table_data.append(row_data)
+            df = pd.DataFrame(table_data[1:], columns=table_data[0])
 
 
+            sub_wise_data = {}
+            columns = df.columns
+            for sub in subjects:
+                data = get_table_data(df, sub,col_name)
+                sub_wise_data[sub] = data
 
-    
-        
-    return render(request,"main.html",{})
+            error = ""  # Initialize error message
+        except:
+            error = "something went wrong"
+            sub_wise_data = {}  # Clear the dictionary in case of an error
+
+        return render(request, "main.html", {"sub_wise_data": sub_wise_data, "error": error})
+
+    else:
+        return render(request, "main.html", {"sub_wise_data": {}})
+
+
